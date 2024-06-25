b7ab779873b6f0af25a69fc28e9a4bd44d79bf07
William-Mba <w.mba@10ference.com>
2024-06-25 17:56:12 -0400

    cleanup demo

--- projects/ngx-twcss/src/lib/components/group/group.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Group } from "./group.directive";
-
-describe('GroupDirective', () => {
-  it('should create an instance', () => {
-
-    const element = new Group();
-    expect(element).toBeTruthy();
-  });
-});
